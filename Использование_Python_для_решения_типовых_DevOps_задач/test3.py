dcfe870b37d4d99eb2fb06627d8e78b0d0dc6975
Vyacheslav <tech@breus.pro>
2022-07-29 13:47:55 +0000

    19

--- Использование_Python_для_решения_типовых_DevOps_задач/test3.py
+++ Использование_Python_для_решения_типовых_DevOps_задач/test3.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 
 import os
-import sys
+import sys							# Импорт модуля sys
 
-cmd = sys.argv[1]
+cmd = sys.argv[1]						# Добавление директории как аргумента
 bash_command = ["cd "+cmd, "git status"]
 result = os.popen(' && '.join(bash_command)).read()
-#is_change = False  # Лишняя проверка
+#is_change = False  						# Лишняя проверка
 for result in result.split('\n'):
     if result.find('modified') != -1:
         prepare_result = result.replace('\tmodified:   ', '')
         print(prepare_result)
-#        break # Закомментировал т.к. с этой опцией скрипт прерывается после первого вхождения
+#        break 							# Закомментировал т.к. с этой опцией скрипт прерывается после первого вхождения
