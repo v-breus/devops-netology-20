f674abe1ee2b0332c9d2df4aa871085bacefe9d9
Vyacheslav <tech@breus.pro>
2022-07-29 10:59:01 +0000

    9

---
+++ test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import os
+
+bash_command = ["cd /vagrant/devops-netology-20", "git status"]
+result = os.popen(' && '.join(bash_command)).read()
+#is_change = False  # Лишняя проверка
+for result in result.split('\n'):
+    if result.find('modified') != -1:
+        prepare_result = result.replace('\tmodified:   ', '')
+        print(prepare_result)
+#        break # Закомментировал т.к. с этой опцией скрипт прерывается после первого вхождения
